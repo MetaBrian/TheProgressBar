73940b552fdf5bba757462d7670ff6e705a053c5
MetaBrian <114273788+MetaBrian@users.noreply.github.com>
2024-09-08 03:04:03 -0400

    removed test change of the cleanBar branch

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,6 @@ function Footer() {
     return (
         <div className='footer'>
             <a className="footerLinks" target='_blank' href='https://github.com/MetaBrian'><h2>Github</h2></a>
-            <a className="footerLinks" target='_blank' href='https://github.com/MetaBrian'><h2>Github</h2></a>
             <a className="footerLinks" target='_blank' href='https://www.linkedin.com/in/leonardo-brian-campos/'><h2>LinkedIn</h2></a>
         </div>
     )
